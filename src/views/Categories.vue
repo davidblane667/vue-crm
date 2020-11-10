3dcca3d01bcbaf4f2f65a791ec75c5ea9f61490c
Nikita <davidblane667@mail.ru>
2020-11-10 15:29:47 +0700

    Categories create - ready

---
+++ src/views/Categories.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="page-title">
+      <h3>Категории</h3>
+    </div>
+    <section>
+      <div class="row">
+
+        <CategoryCreate @created="addNewCategory"/>
+        <CategoryEdit/>
+
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import CategoryCreate from '@/components/CategoryCreate'
+  import CategoryEdit from '@/components/CategoryEdit'
+
+  export default {
+    name: 'categories',
+    data: () => ({
+      categories: []
+    }),
+    methods: {
+      addNewCategory(category) {
+        this.categories.push(category)
+      }
+    },
+    components: {
+      CategoryCreate,
+      CategoryEdit
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
